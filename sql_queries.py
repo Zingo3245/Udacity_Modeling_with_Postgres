a62feb03dafdc803b436c8be88b4a3a27cc774e5
Nick Thomas <nickthomas@Nicks-MacBook-Pro.local>
2021-12-11 18:14:33 -0600

    Updated SQL query files
    Do this because ON CONFLICT statements were added

--- sql_queries.py
+++ sql_queries.py
@@ -28,17 +28,17 @@ time_table_create = ("""CREATE TABLE IF NOT EXISTS time(start_time date PRIMARY 
 songplay_table_insert = ("""INSERT INTO songplay(start_time, user_id, level, song_id, artist_id, session_id, location, user_agent) VALUES (%s, %s, %s, %s, %s, %s, %s, %s)
 """)
 
-user_table_insert = ("""INSERT INTO user_(user_id, first_name , last_name, gender, level) VALUES (%s, %s, %s, %s, %s) ON CONFLICT DO NOTHING
+user_table_insert = ("""INSERT INTO user_(user_id, first_name , last_name, gender, level) VALUES (%s, %s, %s, %s, %s) ON CONFLICT (user_id) DO UPDATE SET level = EXCLUDED.level
 """)
 
-song_table_insert = ("""INSERT INTO song(song_id, title, artist_id, year, duration) VALUES (%s, %s, %s, %s, %s)
+song_table_insert = ("""INSERT INTO song(song_id, title, artist_id, year, duration) VALUES (%s, %s, %s, %s, %s) ON CONFLICT (song_id) DO NOTHING
 """)
 
-artist_table_insert = ("""INSERT INTO artist(artist_id, name, location, latitude, longitude) VALUES (%s, %s, %s, %s, %s)
+artist_table_insert = ("""INSERT INTO artist(artist_id, name, location, latitude, longitude) VALUES (%s, %s, %s, %s, %s) ON CONFLICT (artist_id) DO NOTHING
 """)
 
 
-time_table_insert = ("""INSERT INTO time(start_time, hour, day, week, month, year, weekday) VALUES (%s, %s, %s, %s, %s, %s, %s)
+time_table_insert = ("""INSERT INTO time(start_time, hour, day, week, month, year, weekday) VALUES (%s, %s, %s, %s, %s, %s, %s) ON CONFLICT (start_time) DO NOTHING
 """)
 
 # FIND SONGS
